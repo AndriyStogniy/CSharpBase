9993dc22608fbfd5ddda587e4ac4ccf54cb748f0
AndriyStogniy <stogniy.9797@gmail.com>
2021-07-22 15:15:32 +0300

    Added 2 homeworks

---
+++ MyFirstProject/HW1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MyFirstProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+/*
+            //№1 
+            Console.WriteLine("Enter first number");
+            double firstNum = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter second number");
+            double secondNum = double.Parse(Console.ReadLine());
+            double res = firstNum / secondNum;
+
+            //№2
+            //2.1
+            //Console.WriteLine($"Result = {res:#.##}");
+            //2.2
+            Console.WriteLine($"Result = {Math.Round(res, 2)}");
+*/
+            //№3
+            long phoneNumber = 19876543210;
+            Console.WriteLine(string.Format(format: "{0:+##(###) ###-#1-##}", phoneNumber));
+        }
+    }
+}
---
+++ MyFirstProject/HW2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //#1
+            Console.Write("Enter a date (e.g. 10/22/1987): ");
+            DateTime inputtedDate = DateTime.Parse(Console.ReadLine());
+            DateTime independenceDay = new DateTime(1991, 8, 24);
+
+            if (inputtedDate > independenceDay)
+                Console.Write("You were born in independent Ukraine");
+            else
+                Console.Write("You weren't born in independent Ukraine");
+
+            //#2
+            Console.Write($"\nsizeof(int) - {sizeof(int)}");
+
+            //#3
+            int yearNow = DateTime.Now.Year;
+            int yearBorn = inputtedDate.Year;
+            int age = yearNow - yearBorn;
+            Console.WriteLine($"\nAge - {age}");
+            double divRes = (double)age / (double)yearBorn;
+            Console.WriteLine($"Division result - {divRes}");
+
+            //#4
+            Console.Write("\nHave you a job? (y/n)");
+            string answer = Console.ReadLine();
+
+            switch (answer)
+            {
+                case "y":
+                    Console.WriteLine("It so good");
+                    break;
+                case "n":
+                    Console.WriteLine("Find it");
+                    break;
+                default:
+                    Console.WriteLine("Incorrect ansver");
+                    break;
+            }
+
+
+        }
+    }
+}
